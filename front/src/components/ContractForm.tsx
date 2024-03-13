a22f55043fe23db06b1aa78a967c290729bdd9c3
romAYAYA <supernaturalmlady@gmail.com>
2024-03-13 23:47:14 +0500

    couple fixes in post form

--- front/src/components/ContractForm.tsx
+++ front/src/components/ContractForm.tsx
@@ -1,7 +1,7 @@
 import TextField from '@mui/material/TextField'
 import ComboBox from './ComboBox.tsx'
 import { Box, Button } from '@mui/material'
-import { FormEventHandler, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 interface IForm {
@@ -12,67 +12,74 @@ interface IForm {
   date: string
 }
 
-
 const ContractForm = () => {
   const [form, setForm] = useState<IForm>({
-      comment: '',
-      total: 0,
-      contract: 'privet',
-      file_path: null,
-      date: '12-12-2002'
-    }
-  )
+    comment: '',
+    total: 0,
+    contract: '',
+    file_path: null,
+    date: ''
+  })
 
-  const postData = (event: any) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0]
+    setForm({ ...form, file_path: file || null })
+  }
+
+  const postData = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
+    const formData = new FormData()
+    formData.append('comment', form.comment)
+    formData.append('total', form.total.toString())
+    formData.append('contract', form.contract)
+    formData.append('date', new Date().toISOString().split('T')[0])
+    if (form.file_path) {
+      formData.append('file_path', form.file_path)
+    }
 
-    const res = axios.post('http://127.0.0.1:8000/api/post/contract/', {
+    try {
+      const res = await axios.post('http://127.0.0.1:8000/api/post/contract/', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
-      }
-    )
-
-
+      })
+      console.log(res)
+    } catch (error) {
+      console.error(`Error: ${ error }`)
+    }
   }
-
 
   return (
     <>
-      <form onSubmit={ postData } encType="multipartFormData">
+      <form onSubmit={ postData }>
         <Box sx={ { display: 'flex', flexDirection: 'column', gap: '5px', width: '200px' } }>
-          <ComboBox />
-          <Button
-            variant="contained"
-            component="label"
-          >
-            Upload Avatar
-            <input type="file" accept="image/*" hidden
-            />
+          <ComboBox/>
+          <Button variant="contained" component="label">
+            Upload File
+            <input onChange={ handleFileChange } type="file" name="file_path" hidden/>
           </Button>
-          <TextField onChange={ (event) => setForm({ ...form, comment: event.target.value }) } label="Comment"
+          <TextField value={ form.comment }
+                     onChange={ handleInputChange }
+                     name="comment"
+                     label="Comment"
                      variant="standard"/>
-          <TextField onChange={ (event) => setForm({ ...form, total: parseFloat(event.target.value) }) } label="Total"
+          <TextField value={ form.total }
+                     onChange={ handleInputChange }
+                     name="total"
+                     label="Total"
                      type="number"
-                     variant="standard"
-          />
+                     variant="standard"/>
           <Button type="submit">Submit</Button>
         </Box>
-
       </form>
     </>
   )
 }
 
 export default ContractForm
-
-
-
-
-
-
-
-
-
-
